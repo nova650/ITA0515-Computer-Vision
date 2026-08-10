e2ffb552a278cb4821ba732cd2e69084804a859c
Jeswanth Selvam <jeswa@MacBookPro.mshome.net>
2026-08-10 09:44:28 +0530

    Delete all files except Assessment Tools folder

--- EXP.1.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-img = Image.open("C:/Users/ASUS/OneDrive/Documents/computer vision/Bheem.jpg")
-gray = img.convert("L")
-
-img.show()
-gray.show()
-
-gray.save("grayscale_image.jpg")
--- EXP.2.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageFilter
-
-img = Image.open("Bheem.jpg")
-blur = img.filter(ImageFilter.GaussianBlur(5))
-
-img.show()
-blur.show()
-
-blur.save("blur_image.jpg")
